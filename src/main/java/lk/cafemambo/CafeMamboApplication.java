7f988b0bd0fddd70ff738e86d1a22f80382cd4ba
sachinda <sachinda9696@gmail.com>
2021-07-10 00:28:09 +0530

    Initilize Project

---
+++ src/main/java/lk/cafemambo/CafeMamboApplication.java
@@ -0,0 +1,13 @@
+package lk.cafemambo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeMamboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeMamboApplication.class, args);
+	}
+
+}
