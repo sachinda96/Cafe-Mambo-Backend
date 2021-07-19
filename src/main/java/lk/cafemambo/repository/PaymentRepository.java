5f681c9998604fbf04bd07fcba9ede5b7755a74b
sachinda <sachinda9696@gmail.com>
2021-07-19 17:34:33 +0530

    change payment repo

--- src/main/java/lk/cafemambo/repository/PaymentRepository.java
+++ src/main/java/lk/cafemambo/repository/PaymentRepository.java
@@ -1,6 +1,7 @@
 package lk.cafemambo.repository;
 
+import lk.cafemambo.entity.PaymentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PaymentRepository extends JpaRepository<PaymentRepository,String>{
+public interface PaymentRepository extends JpaRepository<PaymentEntity,String>{
 }
